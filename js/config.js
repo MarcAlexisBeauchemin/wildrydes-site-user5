67621cf1dbbfa8f54243294bf1013622aead6b18
EC2 Default User <ec2-user@ip-172-31-8-181.us-east-2.compute.internal>
2020-09-23 09:23:44 +0000

    maj config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://7a2himkjvc.execute-api.us-east-2.amazonaws.com/prod-user5' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7a2himkjvc.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
